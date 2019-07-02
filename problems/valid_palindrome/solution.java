fd8a3db98a7e1cafb4c831280132a9f7da5a1d98
texhnolyzze <ilyaskarimullin1997@gmail.com>
2019-07-02 18:39:36 +0000

    Sync LeetCode submission - Valid Palindrome (java)

---
+++ problems/valid_palindrome/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+        }
+        return true;
+    }
+}
