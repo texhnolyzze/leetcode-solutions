aafed62d22ba91ab33850a1bfbd063592f7f972e
texhnolyzze <ilyaskarimullin1997@gmail.com>
2019-07-09 20:11:48 +0000

    Sync LeetCode submission - Customers Who Never Order (mysql)

---
+++ problems/customers_who_never_order/solution.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT Customers.Name AS Customers FROM Customers WHERE Customers.Id NOT IN (SELECT Customers.Id FROM Customers INNER JOIN Orders ON Customers.Id = Orders.CustomerId);
