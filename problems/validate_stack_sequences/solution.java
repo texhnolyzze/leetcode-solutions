a7056127759876330d8b955d39b5a68c24151fc7
texhnolyzze <ilyaskarimullin1997@gmail.com>
2021-09-08 15:30:47 +0000

    Sync LeetCode submission - Validate Stack Sequences (java)

--- problems/validate_stack_sequences/solution.java
+++ problems/validate_stack_sequences/solution.java
@@ -1,31 +1,26 @@
 class Solution {
-    public static boolean validateStackSequences(int[] pushed, int[] popped) {
-        Set<Integer> alreadyPoppedIndex = new HashSet<>();
-        Map<Integer, Integer> indexOf = new HashMap<>();
-        for (int i = 0; i < pushed.length; i++)
-            indexOf.put(pushed[i], i);
-        int currPoppedIndex = 0;
-        for (int i = 0; i < popped.length; i++) {
-            int nextPopped = popped[i];
-            int nextPoppedIndex = indexOf.get(nextPopped);
-            alreadyPoppedIndex.add(nextPoppedIndex);
-            int p = currPoppedIndex - 1;
-            while (alreadyPoppedIndex.contains(p))
-                p--;
-            if (nextPoppedIndex < p) {
+    public boolean validateStackSequences(
+        final int[] pushed,
+        final int[] popped
+    ) {
+        final Deque<Integer> stack = new LinkedList<>();
+        int pushedIdx = 0;
+        int poppedIdx = 0;
+        while (poppedIdx < popped.length) {
+            final int expectedPop = popped[poppedIdx++];
+            if (!stack.isEmpty() && stack.peek() == expectedPop) {
+                stack.pop();
+                continue;
+            }
+            while (pushedIdx < pushed.length) {
+                final int pushedElem = pushed[pushedIdx++];
+                stack.push(pushedElem);
+                if (pushedElem == expectedPop) {
+                    break;
+                }
+            }
+            if (stack.isEmpty() || stack.pop() != expectedPop) {
                 return false;
-            } else {
-                if (nextPoppedIndex == popped.length - 1) {
-                    currPoppedIndex = popped.length - 1;
-                    for (int j = i + 1; j < popped.length; j++) {
-                        nextPoppedIndex = indexOf.get(popped[j]);
-                        if (nextPoppedIndex > currPoppedIndex)
-                            return false;
-                        currPoppedIndex = nextPoppedIndex;
-                    }
-                    return true;
-                } else 
-                    currPoppedIndex = nextPoppedIndex;
             }
         }
         return true;
